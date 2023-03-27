82035540b6cef07044d6b6517aca4a84d76bc677
alierdemsunar <alierdem.sunar@pinet.com.tr>
2023-03-27 14:09:59 +0300

    Initial commit

--- app/Http/Resources/Portal/Document/DocumentResource.php
+++ app/Http/Resources/Portal/Document/DocumentResource.php
@@ -11,9 +11,6 @@ class DocumentResource extends JsonResource
     {
         return [
             'participant_id' => ['value'=>$this->participant_id, 'type'=>'select'],
-            'file_link' => ['value'=>$this->file_name.'.'.$this->file_extension, 'type'=>'link'],
-            'file_name' => ['value'=>$this->file_name, 'type'=>'text'],
-            'file_extension' => ['value'=>$this->file_extension, 'type'=>'text'],
             'title' => ['value'=>$this->title, 'type'=>'text'],
             'type' => ['value'=>$this->type, 'type'=>'select'],
             'status' => ['value'=>$this->status, 'type'=>'radio'],
