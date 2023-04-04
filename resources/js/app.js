f5ff61b997e0caa3a6822e3cec3495bff63e70e7
Ali Erdem Sunar <alierdem.sunar@pinet.com.tr>
2023-04-04 22:40:57 +0300

    Initial commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import './bootstrap';
+import '@eonasdan/tempus-dominus/dist/js/tempus-dominus.min';
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
--- resources/views/components/input/date.blade.php
+++ resources/views/components/input/date.blade.php
@@ -3,10 +3,15 @@
     <label for="{{ $method }}-{{ $name }}" class="form-label">
         <i class="fa-regular fa-{{ $icon }}"></i> {{ __('common.'.$title) }}
     </label>
-    <input type="date" name="{{ $name }}" class="form-control @error($name)is-invalid @enderror" id="{{ $method }}-{{ $name }}" value="{{ old($name) }}" placeholder="{{ __('common.'.$title) }}" />
+    <input type="text" name="{{ $name }}" class="form-control @error($name)is-invalid @enderror" id="{{ $method }}-{{ $name }}" value="{{ old($name) }}" placeholder="{{ __('common.'.$title) }}" />
     @error($name)
         <div class="invalid-feedback d-block">
             <i class="fa-regular fa-triangle-exclamation"></i> {{ $message }}
         </div>
     @enderror
 </div>
+<script type="module">
+    new TempusDominus(document.getElementById('{{ $method }}-{{ $name }}'), {
+        //put your config here
+    });
+</script>
