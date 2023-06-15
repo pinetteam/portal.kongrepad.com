035e4ed6838b21577e7619bd9fea3a97ef7d5d9e
alierdemsunar <alierdem.sunar@pinet.com.tr>
2023-06-15 15:27:05 +0300

    Initial commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,55 +1,5 @@
 import './bootstrap';
-import TempusDominus from '@eonasdan/tempus-dominus';
-
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
-// Tarih seçiciyi başlatan fonksiyon
-function initializeDatePicker(element) {
-    const datepicker = new TempusDominus(element, {
-        format: 'YYYY-MM-DD',
-        buttons: {
-            showToday: true,
-        },
-    });
-}
-
-// Saat seçiciyi başlatan fonksiyon
-function initializeTimePicker(element) {
-    const timepicker = new TempusDominus(element, {
-        format: 'HH:mm',
-        buttons: {
-            showToday: false,
-            showClear: true,
-        },
-    });
-}
-
-// Tarih ve saat seçiciyi başlatan fonksiyon
-function initializeDateTimePicker(element) {
-    const datetimepicker = new TempusDominus(element, {
-        format: 'YYYY-MM-DD HH:mm',
-        buttons: {
-            showToday: true,
-            showClear: true,
-        },
-    });
-}
-
-// Tüm datetime picker alanlarını seçip ilgili başlatma fonksiyonunu çağıran fonksiyon
-function initializeDateTimePickers() {
-    document.querySelectorAll('.datepicker').forEach((element) => {
-        initializeDatePicker(element);
-    });
-
-    document.querySelectorAll('.timepicker').forEach((element) => {
-        initializeTimePicker(element);
-    });
-
-    document.querySelectorAll('.datetimepicker').forEach((element) => {
-        initializeDateTimePicker(element);
-    });
-}
-
-// İlgili fonksiyonu çağırarak datetime picker alanlarını başlatın
-initializeDateTimePickers();
+import moment from 'moment';
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    optimizeDeps: {
+        include: ['@eonasdan/tempus-dominus', 'moment'],
+    },
     plugins: [
         laravel({
             input: [
