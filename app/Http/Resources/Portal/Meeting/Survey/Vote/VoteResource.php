83d2cc5551bb2a468937e280840f660dc224ad96
berilsevdi <=>
2023-08-09 22:02:48 +0300

    Seeders

---
+++ app/Http/Resources/Portal/Meeting/Survey/Vote/VoteResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources\Portal\Meeting\Survey\Vote;
+
+use App\Models\Meeting\Survey\Survey;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class VoteResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'survey_id' => ['value'=>$this->survey_id, 'type'=>'hidden'],
+            'sort_order' => ['value'=>$this->sort_order, 'type'=>'number'],
+            'question' => ['value'=>$this->question, 'type'=>'text'],
+            'option' => ['value'=>$this->option, 'type'=>'text'],
+            'status' => ['value'=>$this->status, 'type'=>'radio'],
+            'route' => route('portal.question.update', [Survey::where('id',$this->survey_id)->first()->meeting_id, $this->survey_id, $this->id]),
+        ];
+    }
+}
