bf1507a241d2b1772e2ee7b71f2c579420b17fae
berilsevdi <=>
2023-10-10 03:16:00 +0300

    Seeder

---
+++ database/seeders/MeetingHallProgramChairSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Meeting\Hall\Program\Chair\Chair;
+use Illuminate\Database\Seeder;
+
+class MeetingHallProgramChairSeeder extends Seeder
+{
+    public function run(): void
+    {
+    Chair::insert(
+        [
+
+        ]);
+    }
+}
