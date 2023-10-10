19fd9a9cbdcd20fc2a92a774d499788a949d6b85
berilsevdi <=>
2023-10-10 05:17:44 +0300

    Chair Seeder

--- database/seeders/MeetingHallProgramChairSeeder.php
+++ database/seeders/MeetingHallProgramChairSeeder.php
@@ -11,6 +11,10 @@ class MeetingHallProgramChairSeeder extends Seeder
     {
         Chair::insert([
             [
+                'program_id' => 7,
+                'chair_id' => '129',
+            ],
+            [
                 'program_id' => 8,
                 'chair_id' => '108',
             ],
@@ -65,6 +69,174 @@ class MeetingHallProgramChairSeeder extends Seeder
             [
                 'program_id' => 18,
                 'chair_id' => '121',
+            ],
+            [
+                'program_id' => 21,
+                'chair_id' => '122',
+            ],
+            [
+                'program_id' => 21,
+                'chair_id' => '123',
+            ],
+            [
+                'program_id' => 21,
+                'chair_id' => '124',
+            ],
+            [
+                'program_id' => 22,
+                'chair_id' => '125',
+            ],
+            [
+                'program_id' => 24,
+                'chair_id' => '126',
+            ],
+            [
+                'program_id' => 24,
+                'chair_id' => '127',
+            ],
+            [
+                'program_id' => 25,
+                'chair_id' => '128',
+            ],
+            [
+                'program_id' => 27,
+                'chair_id' => '130',
+            ],
+            [
+                'program_id' => 28,
+                'chair_id' => '131',
+            ],
+            [
+                'program_id' => 28,
+                'chair_id' => '132',
+            ],
+            [
+                'program_id' => 30,
+                'chair_id' => '133',
+            ],
+            [
+                'program_id' => 34,
+                'chair_id' => '134',
+            ],
+            [
+                'program_id' => 34,
+                'chair_id' => '135',
+            ],
+            [
+                'program_id' => 34,
+                'chair_id' => '136',
+            ],
+            [
+                'program_id' => 34,
+                'chair_id' => '137',
+            ],
+            [
+                'program_id' => 36,
+                'chair_id' => '138',
+            ],
+            [
+                'program_id' => 37,
+                'chair_id' => '139',
+            ],
+            [
+                'program_id' => 37,
+                'chair_id' => '140',
+            ],
+            [
+                'program_id' => 37,
+                'chair_id' => '141',
+            ],
+            [
+                'program_id' => 39,
+                'chair_id' => '142',
+            ],
+            [
+                'program_id' => 40,
+                'chair_id' => '143',
+            ],
+            [
+                'program_id' => 40,
+                'chair_id' => '144',
+            ],
+            [
+                'program_id' => 40,
+                'chair_id' => '145',
+            ],
+            [
+                'program_id' => 43,
+                'chair_id' => '146',
+            ],
+            [
+                'program_id' => 43,
+                'chair_id' => '147',
+            ],
+            [
+                'program_id' => 43,
+                'chair_id' => '148',
+            ],
+            [
+                'program_id' => 46,
+                'chair_id' => '149',
+            ],
+            [
+                'program_id' => 48,
+                'chair_id' => '150',
+            ],
+            [
+                'program_id' => 48,
+                'chair_id' => '151',
+            ],
+            [
+                'program_id' => 48,
+                'chair_id' => '152',
+            ],
+            [
+                'program_id' => 49,
+                'chair_id' => '153',
+            ],
+            [
+                'program_id' => 49,
+                'chair_id' => '154',
+            ],
+            [
+                'program_id' => 49,
+                'chair_id' => '155',
+            ],
+            [
+                'program_id' => 50,
+                'chair_id' => '156',
+            ],
+            [
+                'program_id' => 50,
+                'chair_id' => '157',
+            ],
+            [
+                'program_id' => 50,
+                'chair_id' => '158',
+            ],
+            [
+                'program_id' => 51,
+                'chair_id' => '159',
+            ],
+            [
+                'program_id' => 51,
+                'chair_id' => '160',
+            ],
+            [
+                'program_id' => 51,
+                'chair_id' => '161',
+            ],
+            [
+                'program_id' => 52,
+                'chair_id' => '162',
+            ],
+            [
+                'program_id' => 52,
+                'chair_id' => '163',
+            ],
+            [
+                'program_id' => 52,
+                'chair_id' => '164',
             ],
         ]);
     }
