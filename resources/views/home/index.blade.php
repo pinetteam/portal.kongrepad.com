25be16c9dacf9211e16053ade496420af433d68c
berilsevdi <=>
2024-01-27 15:44:29 +0300

    Home Page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -36,28 +36,15 @@
                         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
                         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 4"></button>
                     </div>
                     <div class="carousel-inner align-items-center">
                         <div class="carousel-item active align-items-center" data-bs-interval="10000">
                             <div class="container align-items-center mb-4">
-                                <h1 class="border-bottom border-dark-subtle text-white text-center">What is KongrePad?</h1>
+                                <h1 class="border-bottom border-dark-subtle text-white text-center">{{ trans('common.what_is_kongrepad2') }}</h1>
                                     <div class="row pt-2 align-items-center text-center">
                                         <div class="btn-group w-100 mb-4">
                                         <div class="col">
-                                            <h5 class="text-white">KongrePad allows you to control your congress via a single phone application without the need for an extra device. Thanks to its database structure, it can be used in more than one congress at the same time.</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item" data-bs-interval="2000">
-                            <div class="container align-items-center mb-4">
-                                <h1 class="border-bottom border-dark-subtle text-white text-center">Advantages</h1>
-                                <div class="row pt-2 align-items-center text-center">
-                                    <div class="btn-group w-100 mb-4">
-                                        <div class="col">
-                                            <h5 class="text-white">The timer, session chair or speaker name, document, keypad and debate voting results can be dynamically displayed on the screens. The font, font size and background of each screen can be changed.</h5>
+                                            <h5 class="text-white">{{ trans('common.kongrepad_is_an_application_platform_with_live_key2') }}</h5>
                                         </div>
                                     </div>
                                 </div>
@@ -82,7 +69,7 @@
                         </div>
                         <div class="carousel-item" data-bs-interval="2000">
                             <div class="container align-items-center">
-                                <h1 class="border-bottom border-dark-subtle text-white text-center mb-0">Download KongrePad</h1>
+                                <h1 class="border-bottom border-dark-subtle text-white text-center mb-0">{{ trans('common.download_kongrepad') }}</h1>
                                 <div class="row">
                                     <div class="col">
                                         <a href="https://apps.apple.com/tr/app/kongrepad/id6463897045" target="_blank" title="KongrePad AppStore">
@@ -116,22 +103,86 @@
                 </div>
             </div>
         </div>
-        <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="row">
+            <h3 class="text-light">{{ trans('common.empower_your_events_elevate_your_experience_stream') }}</h3>
+            <hr class="text-light">
+        </div>
+        <div class="row row-cols-1 row-cols-md-3 g-4 mb-4">
             <div class="col">
                 <div class="card h-100 bg-color2 text-white shadow-lg">
-                    <div class="h1 m-3"><span class="fa-duotone fa-square-poll-horizontal fa-fade"></span></div>
+                    <div class="h1 m-3"><span class="fa-duotone fa-cloud-check fa-fade"></span></div>
                     <div class="card-body">
-                        <h5 class="card-title">Surveys</h5>
-                        <p class="card-text">Surveys can be conducted and detailed survey reports can be obtained. All users can answer the surveys via their phone.</p>
+                        <h5 class="card-title">{{ trans('common.cloud_based') }}</h5>
+                        <p class="card-text">{{ trans('common.manage_your_congress_from_anywhere_with_its_web_ap') }}</p>
                     </div>
                 </div>
             </div>
             <div class="col">
-                <div class="card h-100 bg-color1 text-white shadow-sm">
+                <div class="card h-100 bg-color2 text-white shadow-lg">
+                    <div class="h1 m-3"><span class="fa-duotone fa-mobile-signal-out fa-fade"></span></div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ trans('common.mobile_app') }}</h5>
+                        <p class="card-text">{{ trans('common.with_kongrepad_participants_can_access_the_congres') }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 bg-color2 text-white shadow-lg">
+                    <div class="h1 m-3"><span class="fa-duotone fa-user-tie fa-fade"></span></div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ trans('common.easy_to_use') }}</h5>
+                        <p class="card-text">{{ trans('common.thanks_to_its_userfriendly_interface_it_is_now_ver') }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 bg-color2 text-white shadow-lg">
+                    <div class="h1 m-3"><span class="fa-duotone fa-people-roof fa-fade"></span></div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ trans('common.multiple_users') }}</h5>
+                        <p class="card-text">{{ trans('common.add_as_many_users_as_you_want_to_your_account_and') }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 bg-color2 text-white shadow-lg">
+                    <div class="h1 m-3"><span class="fa-duotone fa-coin fa-fade"></span></div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ trans('common.affordable') }}</h5>
+                        <p class="card-text">{{ trans('common.experience_highquality_functionality_without_break') }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 bg-color2 text-white shadow-lg">
+                    <div class="h1 m-3"><span class="fa-duotone fa-video fa-fade"></span></div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ trans('common.video_tutorials') }}</h5>
+                        <p class="card-text">{{ trans('common.take_advantage_of_free_tutorials_whenever_you_need') }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <h3 class="text-light mt-3">{{ trans('common.check_out_the_services_kongrepad_offers') }}</h3>
+            <hr class="text-light">
+        </div>
+        <div class="row row-cols-1 row-cols-md-4 g-4">
+            <div class="col">
+                <div class="card h-100 bg-color2 text-white shadow-lg">
+                    <div class="h1 m-3"><span class="fa-duotone fa-square-poll-horizontal fa-fade"></span></div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ trans('common.follow_live_document') }}</h5>
+                        <p class="card-text">{{ trans('common.the_user_can_watch_the_speakers_onscreen_presentat') }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 bg-color2 text-white shadow-sm">
                     <div class="h1 m-3"><span class="fa-duotone fa-fade fa-calendar-week text-white"></span></div>
                     <div class="card-body">
-                        <h5 class="card-title">Keypads</h5>
-                        <p class="card-text">Keypads can be made and detailed keypad reports can be obtained. All users can answer the question via their phone.</p>
+                        <h5 class="card-title">{{ trans('common.keypad_questions') }}</h5>
+                        <p class="card-text">{{ trans('common.publish_keypad_questions_that_users_can_answer_ins') }}</p>
                     </div>
                 </div>
             </div>
@@ -139,17 +190,17 @@
                 <div class="card h-100 bg-color2 text-white shadow-lg">
                     <div class="h1 m-3"><span class="fa-duotone fa-question fa-fade"></span></div>
                     <div class="card-body">
-                        <h5 class="card-title">Asking Question</h5>
-                        <p class="card-text">Participants can ask questions to the speaker anonymously or under their own names during the session.</p>
+                        <h5 class="card-title">{{ trans('common.asking_question') }}</h5>
+                        <p class="card-text">{{ trans('common.the_user_can_ask_questions_to_the_speaker_via_hish') }}</p>
                     </div>
                 </div>
             </div>
             <div class="col">
-                <div class="card h-100 bg-color1 text-white shadow-sm">
+                <div class="card h-100 bg-color2 text-white shadow-sm">
                     <div class="h1 m-3"><span class="fa-duotone fa-podium-star fa-fade"></span></div>
                     <div class="card-body">
-                        <h5 class="card-title">Debate</h5>
-                        <p class="card-text">Debates can be created. You can create any teams you want. Participants can vote via the application.</p>
+                        <h5 class="card-title">{{ trans('common.debate_voting') }}</h5>
+                        <p class="card-text">{{ trans('common.offer_debate_voting_so_users_can_vote_for_the_team') }}</p>
                     </div>
                 </div>
             </div>
@@ -157,8 +208,123 @@
                 <div class="card h-100 bg-color2 text-white shadow-lg">
                     <div class="h1 m-3"><span class="fa-duotone fa-chart-user fa-fade"></span></div>
                     <div class="card-body">
-                        <h5 class="card-title">Reports</h5>
-                        <p class="card-text">Thanks to the QR code scanning system, attendance at the session can be made and attendance can be seen. Thanks to the log system, user movements, keypad and debate participation and sessions can be reported. Surveys can be conducted and detailed survey reports can be obtained.</p>
+                        <h5 class="card-title">{{ trans('common.reports2') }}</h5>
+                        <p class="card-text">{{ trans('common.thanks_to_the_qr_code_scanning_system_attendance_a') }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 bg-color2 text-white shadow-sm">
+                    <div class="h1 m-3"><span class="fa-duotone fa-hand fa-fade"></span></div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ trans('common.document_request') }}</h5>
+                        <p class="card-text">{{ trans('common.the_user_can_request_the_desired_documents_via_ema') }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 bg-color2 text-white shadow-sm">
+                    <div class="h1 m-3"><span class="fa-duotone fa-hundred-points fa-fade"></span></div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ trans('common.score_games') }}</h5>
+                        <p class="card-text">{{ trans('common.score_games_can_be_organized_for_the_user_to_colle') }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 bg-color2 text-white shadow-sm">
+                    <div class="h1 m-3"><span class="fa-duotone fa-screen-users fa-fade"></span></div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ trans('common.screen_management') }}</h5>
+                        <p class="card-text">{{ trans('common.the_timer_session_chair_or_speaker_name_document_k') }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <h3 class="text-light mt-3 mt-5">{{ trans('common.frequently_asked_questions') }}</h3>
+            <hr class="text-light">
+        </div>
+        <div class="row">
+            <div class="accordion accordion-flush text-white" id="accordionFlushExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed bg-color2 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                            {{ trans('common.what_is_kongrepad') }}
+                        </button>
+                    </h2>
+                    <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">{{ trans('common.kongrepad_is_an_application_platform_with_live_key') }}</div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed bg-color2 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                            {{ trans('common.what_kongrepad_offers') }}
+                        </button>
+                    </h2>
+                    <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">{{ trans('common.t_enables_congress_participants_to_participate_in') }}</div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed bg-color2 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                            {{ trans('common.can_try_kongrepad') }}
+                        </button>
+                    </h2>
+                    <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">{{ trans('common.of_course_you_can_try_kongrepad_with_100_free_cred') }}</div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed bg-color2 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+                            {{ trans('common.how_can_use_kongrepad') }}
+                        </button>
+                    </h2>
+                    <div id="flush-collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">{{ trans('common.thanks_to_the_training_videos_we_have_prepared_eve') }}</div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed bg-color2 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
+                            {{ trans('common.can_manage_multiple_congresses_in_a_single_kongrep') }}
+                        </button>
+                    </h2>
+                    <div id="flush-collapseSix" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">{{ trans('common.yes_you_can_manage_as_many_congresses_as_you_want') }}</div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed bg-color2 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
+                            {{ trans('common.what_is_premium_and_standard_congress') }}
+                        </button>
+                    </h2>
+                    <div id="flush-collapseSeven" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">{{ trans('common.premium_congresses_are_congresses_where_keypad_deb') }}</div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed bg-color2 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseEight" aria-expanded="false" aria-controls="flush-collapseEight">
+                            {{ trans('common.what_is_keypad') }}
+                        </button>
+                    </h2>
+                    <div id="flush-collapseEight" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">{{ trans('common.keypads_are_questions_that_you_can_send_to_all_log') }}</div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed bg-color2 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseNine" aria-expanded="false" aria-controls="flush-collapseNine">
+                            {{ trans('common.what_is_credit') }}
+                        </button>
+                    </h2>
+                    <div id="flush-collapseNine" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">{{ trans('common.each_participants_daily_login_is_deducted_from_you') }}</div>
                     </div>
                 </div>
             </div>
